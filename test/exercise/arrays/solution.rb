b97361fdf14398a4e47c756795a7cdcae3d0ca8a
Melixoff <fanthemine@gmail.com>
2022-10-27 15:56:56 +0500

    Solved "replace" task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,19 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+
+        def findMax(array)
+          maxValue = array[0]
+          for el in array
+            if el>maxValue
+              maxValue=el
+            end
+          end
+          return maxValue
+        end
+
+        return array.map{|el| el>0 ? findMax(array) : el}
+        
       end
 
       def search(_array, _query)
