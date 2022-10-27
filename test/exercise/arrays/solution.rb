e105edadb94d7b2ba3ff1b52c54051384ad8dea4
Melixoff <fanthemine@gmail.com>
2022-10-27 17:55:46 +0500

    Solved "bin search" task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -17,8 +17,36 @@ module Exercise
         
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.length == 0
+        leftEdge = 0
+        prevRightEdge = -1
+        prevLeftEdge = -1
+        rightEdge = array.length-1
+        while true
+          centre = (leftEdge+rightEdge)/2
+          if array[leftEdge] == query
+            return leftEdge
+          elsif array[rightEdge] == query
+            return rightEdge
+          elsif array[centre]>query
+            rightEdge = centre
+            if rightEdge == prevRightEdge
+              return -1
+            else
+              prevRightEdge = centre
+            end
+          elsif array[centre]<query
+            leftEdge = centre
+            if leftEdge == prevLeftEdge
+              return -1
+            else
+              prevLeftEdge = centre
+            end
+          elsif array[centre]==query
+            return centre
+          end
+        end
       end
     end
   end
