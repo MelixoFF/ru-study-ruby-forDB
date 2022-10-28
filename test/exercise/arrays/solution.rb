2f920be21b3cb9cb880e7e26d5e898059eec6915
Melixoff <fanthemine@gmail.com>
2022-10-28 20:37:44 +0500

    Renamed accordig to Naming_Conventions and made a recursive solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,50 +3,52 @@ module Exercise
     class << self
       def replace(array)
 
-        def findMax(array)
-          maxValue = array[0]
+        def find_max(array)
+          max_value = array[0]
           for el in array
-            if el>maxValue
-              maxValue=el
+            if el>max_value
+              max_value=el
             end
           end
-          return maxValue
+          return max_value
         end
 
-        return array.map{|el| el>0 ? findMax(array) : el}
+        return array.map{|el| el>0 ? find_max(array) : el}
         
       end
 
-      def search(array, query)
-        return -1 if array.length == 0
-        leftEdge = 0
-        prevRightEdge = -1
-        prevLeftEdge = -1
-        rightEdge = array.length-1
-        while true
-          centre = (leftEdge+rightEdge)/2
-          if array[leftEdge] == query
-            return leftEdge
-          elsif array[rightEdge] == query
-            return rightEdge
-          elsif array[centre]>query
-            rightEdge = centre
-            if rightEdge == prevRightEdge
-              return -1
-            else
-              prevRightEdge = centre
-            end
-          elsif array[centre]<query
-            leftEdge = centre
-            if leftEdge == prevLeftEdge
-              return -1
-            else
-              prevLeftEdge = centre
-            end
-          elsif array[centre]==query
-            return centre
-          end
+      def search(array, query, left_edge=nil, right_edge=nil)
+        if left_edge == nil
+          left_edge = 0
         end
+        if right_edge == nil
+          right_edge = array.length - 1
+        end
+        centre = (left_edge+right_edge)/2
+        if array.length == 0 
+          return -1 
+        end
+        if array[left_edge] == query 
+          return left_edge 
+        end
+        if array[right_edge] == query 
+          return right_edge
+        end
+        if array[centre]==query 
+          return centre
+        end
+        if right_edge-left_edge<=1 
+          return -1
+        end
+
+        if array[centre]>query
+          right_edge = centre
+          return search(array, query, left_edge, right_edge)
+        end
+        if array[centre]<query
+          left_edge = centre
+          return search(array, query, left_edge, right_edge)
+        end 
       end
     end
   end
