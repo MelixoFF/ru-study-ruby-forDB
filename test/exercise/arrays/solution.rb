4f52f31a58deb1c7d0e09e895fe1bfe775e1f259
Melixoff <fanthemine@gmail.com>
2022-10-31 19:46:42 +0500

    completed the task and fixed previous problems

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,54 +1,27 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)
 
-        def find_max(array)
-          max_value = array[0]
-          for el in array
-            if el>max_value
-              max_value=el
-            end
+      def find_max(array)
+        max_value = array[0]
+        for el in array
+          if el>max_value
+            max_value=el
           end
-          return max_value
         end
-
-        return array.map{|el| el>0 ? find_max(array) : el}
-        
+        return max_value
       end
 
-      def search(array, query, left_edge=nil, right_edge=nil)
-        if left_edge == nil
-          left_edge = 0
-        end
-        if right_edge == nil
-          right_edge = array.length - 1
-        end
-        centre = (left_edge+right_edge)/2
-        if array.length == 0 
-          return -1 
-        end
-        if array[left_edge] == query 
-          return left_edge 
-        end
-        if array[right_edge] == query 
-          return right_edge
-        end
-        if array[centre]==query 
-          return centre
-        end
-        if right_edge-left_edge<=1 
-          return -1
-        end
+      def replace(array)
+        return array.map{|el| el>0 ? find_max(array) : el}
+      end
 
-        if array[centre]>query
-          right_edge = centre
-          return search(array, query, left_edge, right_edge)
-        end
-        if array[centre]<query
-          left_edge = centre
-          return search(array, query, left_edge, right_edge)
-        end 
+      def search(array, query, left_edge=0)
+        return left_edge if array[0] == query
+        return -1 if array.length <= 1
+        centre = (0+(array.length-1))/2
+        return array[centre]>=query ? search(array[0,centre], query, left_edge)
+        : search(array[centre+1, array.length-(centre+1)], query, left_edge+centre+1)
       end
     end
   end
