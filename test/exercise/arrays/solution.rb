f7e84dc1d59d99ea114a6a8dfa412f2dc951ea6f
Melixoff <fanthemine@gmail.com>
2022-11-02 13:16:59 +0500

    fixed minor decor problems and rewrote ternary into ifelse

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -5,23 +5,26 @@ module Exercise
       def find_max(array)
         max_value = array[0]
         for el in array
-          if el>max_value
-            max_value=el
+          if el > max_value
+            max_value = el
           end
         end
         return max_value
       end
 
       def replace(array)
-        return array.map{|el| el>0 ? find_max(array) : el}
+        return array.map{|el| el > 0 ? find_max(array) : el}
       end
 
       def search(array, query, left_edge=0)
         return left_edge if array[0] == query
         return -1 if array.length <= 1
         centre = (0+(array.length-1))/2
-        return array[centre]>=query ? search(array[0,centre], query, left_edge)
-        : search(array[centre+1, array.length-(centre+1)], query, left_edge+centre+1)
+        if array[centre] >= query
+          return search(array[0,centre], query, left_edge)
+        else
+          return search(array[centre+1, array.length-(centre+1)], query, left_edge+centre+1)
+        end
       end
     end
   end
